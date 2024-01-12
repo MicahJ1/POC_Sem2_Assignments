8efc7c175ff62971bb042acfc20df6ebadb4a4b4
Micah Jackson <jacksmic@warren.k12.in.us>
2024-01-12 12:11:50 -0500

    assignment

---
+++ 01_Strings/05_Search Strings/Assignment.py
@@ -0,0 +1,9 @@
+text = input("")
+
+result = text.find("the")
+
+if result == -1:
+    print("The word the is not in the string")
+else:
+    print("The word the is in the string")
+    print("it is located at", result)
