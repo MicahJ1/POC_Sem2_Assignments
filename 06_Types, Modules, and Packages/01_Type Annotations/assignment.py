dec60425a53d75d369f449bd5f77f72cbfbefa30
Micah Jackson <jacksmic@warren.k12.in.us>
2024-02-06 12:28:50 -0500

    assignment.py

--- 06_Types, Modules, and Packages/01_Type Annotations/assignment.py
+++ 06_Types, Modules, and Packages/01_Type Annotations/assignment.py
@@ -2,7 +2,7 @@
 #variables types are float, function return type is a float
 
 def average(a, b):
-    return (a + b) / 2
+    return (a + b) / 2 
 
 number1 = float(input("Enter a decimal number: "))
 number2 = float(input("Enter another decimal number: "))
